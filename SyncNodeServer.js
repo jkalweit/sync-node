2b9fc6319c300ade5c86004f39a6ddc82db6289b
Justin Kalweit <kalweit@alumni.duke.edu>
2015-11-30 17:47:00 -0500

    Initial Commit

---
+++ SyncNodeServer.js
@@ -0,0 +1,118 @@
+/// <reference path='./typings/tsd.d.ts' />
+var fs = require('fs');
+var path = require('path');
+var Response = (function () {
+    function Response(requestGuid, data) {
+        this.requestGuid = requestGuid;
+        this.stamp = new Date();
+        this.data = data;
+    }
+    return Response;
+})();
+var SyncNodeServer = (function () {
+    function SyncNodeServer(namespace, io, defaultData) {
+        var _this = this;
+        if (defaultData === void 0) { defaultData = {}; }
+        this.namespace = namespace;
+        this.io = io;
+        this.get(function (data) {
+            _this.data = data;
+            if (!_this.data) {
+                _this.data = JSON.parse(JSON.stringify(defaultData)); // Use a copy for immutability
+            }
+            _this.start();
+        });
+    }
+    SyncNodeServer.prototype.doMerge = function (obj, merge) {
+        var _this = this;
+        if (typeof merge !== 'object')
+            return merge;
+        Object.keys(merge).forEach(function (key) {
+            if (key === 'lastModified' && obj[key] > merge[key]) {
+                console.error('Server version lastModified GREATER THAN merge lastModified', obj[key], merge[key]);
+            }
+            if (key === 'meta') {
+            }
+            else if (key === '__remove') {
+                delete obj[merge[key]];
+            }
+            else {
+                var nextObj = (obj[key] || {});
+                obj[key] = _this.doMerge(nextObj, merge[key]);
+            }
+        });
+        return obj;
+    };
+    SyncNodeServer.prototype.resetData = function (newData) {
+        this.data = JSON.parse(JSON.stringify(newData)); // Use a copy for immutability
+        this.persist();
+        this.ioNamespace.emit('latest', this.data);
+    };
+    SyncNodeServer.prototype.start = function () {
+        var _this = this;
+        this.ioNamespace = this.io.of('/' + this.namespace);
+        this.ioNamespace.on('connection', function (socket) {
+            console.log('someone connected to ' + _this.namespace);
+            socket.on('getlatest', function (clientLastModified) {
+                console.log('getlatest', _this.data.lastModified, clientLastModified);
+                if (!clientLastModified || clientLastModified < _this.data.lastModified) {
+                    socket.emit('latest', _this.data);
+                }
+                else {
+                    console.log('already has latest.');
+                    socket.emit('latest', null);
+                }
+            });
+            socket.on('update', function (request) {
+                var merge = request.data;
+                _this.doMerge(_this.data, merge);
+                _this.persist();
+                socket.emit('updateResponse', new Response(request.requestGuid, null));
+                socket.broadcast.emit('update', merge);
+                if (_this.onMerge)
+                    _this.onMerge(merge);
+            });
+        });
+    };
+    SyncNodeServer.prototype.get = function (callback) {
+        var path = this.buildFilePath();
+        fs.readFile(path, 'utf8', function (err, data) {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    callback(null);
+                }
+                else {
+                    console.error('Failed to read ' + path + ': ' + err);
+                    callback(null);
+                }
+            }
+            else {
+                callback(JSON.parse(data));
+            }
+        });
+    };
+    SyncNodeServer.prototype.persist = function () {
+        var _this = this;
+        var path = this.buildFilePath();
+        console.log(path);
+        fs.mkdir(this.directory, null, function (err) {
+            if (err) {
+                // ignore the error if the folder already exists
+                if (err.code != 'EEXIST') {
+                    console.error('Failed to create folder ' + _this.directory + ': ' + err);
+                    return;
+                }
+            }
+            fs.writeFile(path, JSON.stringify(_this.data), function (err) {
+                if (err) {
+                    console.error('Failed to write ' + path + ': ' + err);
+                }
+            });
+        });
+    };
+    SyncNodeServer.prototype.buildFilePath = function () {
+        return path.join(this.directory, this.filename + '.json');
+    };
+    return SyncNodeServer;
+})();
+exports.SyncNodeServer = SyncNodeServer;
---
+++ SyncNodeServer.ts
@@ -0,0 +1,148 @@
+/// <reference path='./typings/tsd.d.ts' />
+
+import fs = require('fs');
+import path = require('path');
+import socketio = require('socket.io');
+
+interface Request {
+	requestGuid?: string;
+	stamp?: Date;
+	data?: any;
+}
+
+
+class Response {
+	requestGuid: string;
+	stamp: Date;
+	data: any;
+
+	constructor(requestGuid: string, data?: any) {
+		this.requestGuid = requestGuid;
+		this.stamp = new Date();
+		this.data = data;
+	}
+}
+
+export class SyncNodeServer {
+	namespace: string;
+	filename: string;
+    	directory: string;
+	io: SocketIO.Server;
+	ioNamespace: SocketIO.Namespace;
+	data: any;
+	onMerge: (merge: any) => void;
+
+	constructor(namespace: string, io: SocketIO.Server, defaultData: any = {}) {
+		this.namespace  = namespace;
+		this.io = io;
+
+		this.get((data: any) => {
+			this.data = data;
+			if(!this.data) {
+				this.data = JSON.parse(JSON.stringify(defaultData)); // Use a copy for immutability
+			}
+			this.start();
+		});
+	}
+
+	doMerge(obj: any, merge: any) {
+
+
+
+
+		if (typeof merge !== 'object') return merge;
+		Object.keys(merge).forEach((key: string) => {
+			if(key === 'lastModified' && obj[key] as any > merge[key]) {
+				console.error('Server version lastModified GREATER THAN merge lastModified', obj[key], merge[key]);
+			}
+			if(key === 'meta') {
+
+			} else if (key === '__remove') {
+				delete obj[merge[key]];
+			} else {
+				var nextObj = (obj[key] || {}) as any;
+
+				obj[key] = this.doMerge(nextObj, merge[key]);
+			}
+		});
+		return obj;
+	}
+	resetData(newData: any) {
+		this.data = JSON.parse(JSON.stringify(newData)); // Use a copy for immutability
+		this.persist();
+		this.ioNamespace.emit('latest', this.data);
+	}
+	start() {
+		this.ioNamespace = this.io.of('/' + this.namespace);
+		this.ioNamespace.on('connection', (socket: SocketIO.Socket) => {
+			console.log('someone connected to ' + this.namespace);
+
+			socket.on('getlatest', (clientLastModified: string) => {
+				console.log('getlatest', this.data.lastModified, clientLastModified);
+				if (!clientLastModified || clientLastModified < this.data.lastModified) {
+					socket.emit('latest', this.data);
+				} else {
+					console.log('already has latest.');
+					socket.emit('latest', null);
+				}
+			});
+
+
+
+			socket.on('update', (request: Request) => {
+				var merge = request.data;
+				this.doMerge(this.data, merge);
+				this.persist();
+				socket.emit('updateResponse', new Response(request.requestGuid, null));
+				socket.broadcast.emit('update', merge);
+
+
+				if(this.onMerge) this.onMerge(merge);
+			});
+		});
+	}
+
+
+
+    get(callback: (data: any) => void) {
+        var path = this.buildFilePath();
+
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    callback(null);
+                } else {
+                    console.error('Failed to read ' + path + ': ' + err);
+                    callback(null);
+                }
+            } else {
+                callback(JSON.parse(data));
+            }
+        });
+    }
+
+    persist() {
+        var path = this.buildFilePath();
+        console.log(path);
+        fs.mkdir(this.directory, null, (err) => {
+            if (err) {
+                // ignore the error if the folder already exists
+                if (err.code != 'EEXIST') {
+                    console.error('Failed to create folder ' + this.directory + ': ' + err);
+                    return;
+                }
+            }
+
+            fs.writeFile(path, JSON.stringify(this.data), (err) => {
+                if (err) {
+                    console.error('Failed to write ' + path + ': ' + err);
+                }
+            });
+        });
+    }
+
+    buildFilePath(): string {
+        return path.join(this.directory, this.filename + '.json');
+    }
+
+}
